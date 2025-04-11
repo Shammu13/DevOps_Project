68d4b29dcb64820e938b73b0ee72a785b6b200ca
Shammu13 <s2r2learn@gmail.com>
2025-04-11 11:59:09 +0100

    Added M1

--- src/main/java/Basic/First_Program.java
+++ src/main/java/Basic/First_Program.java
@@ -3,8 +3,12 @@ package Basic;
 public class First_Program {
 
 	public static void main(String[] args) {
-		System.out.println("This is my 1st Jave Program");
+		System.out.println("This is my 1st Java Program");
 
 	}
-
+// Code change for branch C1
+	public void m1()
+	{
+	System.out.println("This a side branch change for C1");
+	}
 }
---
+++ src/main/java/Basic/SecondProgram.java
@@ -0,0 +1,15 @@
+package Basic;
+
+public class SecondProgram {
+
+	public static void main(String[] args) {
+		System.out.println("This is my 2nd Jave Program");
+
+	}
+	//from branch b2
+	public void m1()
+	{
+		System.out.println("This is branch change - b2");
+	}
+
+}
